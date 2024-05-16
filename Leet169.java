c0ca174f6a7be35eddc3b3c07fa61b1cb30de105
ABHAY <107785681+abhay8298@users.noreply.github.com>
2024-05-16 23:43:29 +0530

    Create Leet169.java

---
+++ Leet169.java
@@ -0,0 +1,22 @@
+// Majority Element Easy
+// TC=O(n) , SC=O(1)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                cand=nums[i];
+            }
+            if(cand==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return cand;
+        
+    }
+}
