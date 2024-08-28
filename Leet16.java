8e054f7017378e902226e46a877b88f3aeec9548
ABHAY <107785681+abhay8298@users.noreply.github.com>
2024-08-28 20:34:36 +0530

    Create Leet16.java

---
+++ Leet16.java
@@ -0,0 +1,26 @@
+// TC = O(n^2)  SC = O(1)
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int res = nums[0]+nums[1]+nums[nums.length-1];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int a = i+1;
+            int b = nums.length-1;
+
+            while(a < b){
+                int currSum = nums[i]+nums[a]+nums[b];
+                if(currSum > target){
+                    b--;
+                }
+                else{
+                    a++;
+                }
+                if(Math.abs(currSum - target) < Math.abs(res-target)){
+                    res = currSum;
+                }
+            }
+        }
+        return res;
+        
+    }
+}
