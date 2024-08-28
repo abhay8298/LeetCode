d04958964546237c909e8b9ad83ae8bb9e35933c
ABHAY <107785681+abhay8298@users.noreply.github.com>
2024-08-28 20:22:24 +0530

    Update Leet6.java

--- Leet6.java
+++ Leet6.java
@@ -1,24 +1,25 @@
 //  TC =O(n) , SC=O(n)
 
+
 class Solution {
     public String convert(String s, int numRows) {
-    String[] ans = new String[numRows];
-    for(int i=0;i<numRows;i++){
-        ans[i] = "";
-    }
-    int i=0;
-    while(i<s.length()){
-        for(int j=0;j<numRows && i<s.length(); j++){
-            ans[j]+=s.charAt(i++);
+        String[] ans  = new String[numRows];
+        for(int i=0;i<numRows;i++){
+            ans[i] = "";
         }
-        for(int j=numRows-2;j>0 && i<s.length(); j--){
-            ans[j]+=s.charAt(i++);
+        int i=0;
+        while(i < s.length()){
+            for(int index = 0;index < numRows && i < s.length();index++){
+                ans[index] += s.charAt(i++);
+            }
+            for(int index = numRows-2;index > 0 && i < s.length();index--){
+                ans[index] += s.charAt(i++);
+            }
         }
-    }
-    String res="";
-    for(String str : ans){
-        res+=str;
-    }
-    return res;
+        String res = "";
+        for(String a : ans){
+            res += a;
+        }
+        return res;
     }
 }
