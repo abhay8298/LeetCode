c871c3b43d2018c1e2f1b169ff64fbf43a4ec323
ABHAY <107785681+abhay8298@users.noreply.github.com>
2024-09-01 12:17:43 +0530

    Create Leet150.java

---
+++ Leet150.java
@@ -0,0 +1,33 @@
+//TC = O(n)  SC = O(n)
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        String op = "+-/*";
+        for(String s : tokens){
+            if(op.indexOf(s) != -1){
+                int d2 = st.pop();
+                int d1 = st.pop();
+                int res = 0;
+                if(s.equals("+")){
+                    res = d1+d2;
+                }
+                else if(s.equals("-")){
+                    res = d1-d2;
+                }
+                else if(s.equals("/")){
+                    res = d1/d2;
+                }
+                else if(s.equals("*")){
+                    res = d1*d2;
+                }
+                st.push(res);
+            }
+            else{
+                st.push(Integer.parseInt(s));
+            }
+            
+        }
+        return st.pop();
+        
+    }
+}
