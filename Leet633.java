b4bfd3d45ce4d784200690573c7ed7d2a928b530
ABHAY <107785681+abhay8298@users.noreply.github.com>
2024-09-03 12:18:42 +0530

    Create Leet633.java

---
+++ Leet633.java
@@ -0,0 +1,19 @@
+
+// TC = O(nb) SC = O(n)
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int high = (int)Math.sqrt(c);
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<=high;i++){
+            hs.add(i*i);
+        }
+        for(int i=0;i<=high;i++){
+            int want = c - (i*i);
+            if(hs.contains(want)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
